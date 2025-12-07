70e05b68e92230fe51261a7bedab8d6eceab2f97
Kaneez <kanzee3011@gmail.com>
2025-12-07 16:33:49 +0530

    fixed all required bugs

--- app/compnents/cardC.jsx
+++ app/compnents/cardC.jsx
@@ -1,16 +1,21 @@
+"use client"
+import { useRouter } from "next/navigation";
 
 
-export default function CardC({image, name, role}){
+
+export default function CardC({key, image, name, role,username}){
+    const router = useRouter();
     return(
         <>
-            <div className="flex flex-col items-center justify-center h-[250px] w-[400px] shadow-[0_0_8px_rgba(0,0,0,0.25)] my-9">
-                <img src={image}
+            <div  key={key} className="md:flex flex-col items-center justify-center h-[250px] w-[400px] shadow-[0_0_8px_rgba(0,0,0,0.25)] my-9 ">
+                <img src={`https://github.com/${username.replace("@", "")}.png`}
                 alt="member" 
                 height={100} 
                 width={100} 
-                className="rounded-[50%] h-[80px] w-[80px]"/>
+                className="rounded-[50%] h-30 w-30"/>
                 <h1 className="text-black font-bold my-2">{name}</h1>
                 <h3 className="text-gray-950">{role}</h3>
+                <h2 className="text-blue-400 cursor-pointer" onClick={()=>router.push(`https://github.com/${username.replace("@", "")}`)} >{username}</h2>
                 </div>
         </>
     )
