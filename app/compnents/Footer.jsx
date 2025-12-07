71abc487351d354e84d416ff2f6b59a0858d39f5
viroo007 <viroopakshabelal@gmail.com>
2025-12-07 17:17:51 +0530

    next.js is routing to layout.js not page.js
    in home page navbar and footer flipped it's places
    
    fixed navbar position
    
    fixed team page erro

--- app/compnents/Footer.jsx
+++ app/compnents/Footer.jsx
@@ -1,36 +1,56 @@
 import Link from "next/link";
 
+export default function Footer() {
+  return (
+    <>
+      <footer className="bg-[oklch(77.034%_0.20078_152.412)] w-full py-12 mt-10">
+        <div className="max-w-7xl mx-auto px-6 grid grid-cols-1 md:grid-cols-3 gap-10">
 
-export default function Footer(){
-    return(
-        <>
-            <div className="bg-[oklch(77.034%_0.20078_152.412)] w-full h-[400px] flex flex-row mt-10">
+         
+          <div className="flex flex-col gap-3">
+            <h2 className="text-white text-2xl font-bold">
+              Sahyadri Open Source Community
+            </h2>
+            <p className="text-white leading-relaxed">
+              Sahyadri College of Engineering and Management,<br />
+              Adyar, Mangalore - 575007 IN
+            </p>
+            <Link href="https://sosc.org.in" className="underline text-white">
+              sosc.org.in
+            </Link>
+          </div>
+
         
-                <div className="h-[90%] flex felx-row">
-
-                    <div className="flex flex-col justify-center  gap-2 ml-10 w-[100%]">
-                        <h2 className="text-white text-2xl text-bold ">Sahyadri Open Source Community</h2>
-                        <h4 className="text-white">Sahyadri College of Engineering and Management, Adyar, Mangalore - 575007 IN</h4>
-                        <Link href="https://sosc.org.in" className="underline">sosc.org.in</Link>
-                    </div>
-
-                    <div className="flex flex-col justify-center items-center w-[140px] gap-2 ml-40">
-                        <h2 className="text-white font-bold">Fallow Us</h2>
-                        <Link href="https://github.com/so-sc" className="underline">GitHub</Link>
-                        <Link href="https://www.instagram.com/sosc.sahyadri/" className="underline">Instagram</Link>
-                        <Link href="https://www.linkedin.com/company/sosc-sahyadri/posts/?feedView=all" className="underline">LinkedIn</Link>
-                        <Link href="https://x.com/sosc_sahyadri?t=y9WZsgmE1Nh5mfq-i6Ne2A&s=08 " className="underline">X</Link>
-                        <Link href="https://www.youtube.com/channel/UCk8nlSMwUT-jhEtamMF-V-w" className="underline">YouTube</Link>
-                    </div>
-
-                    <div className="flex flex-col justify-center items-center w-[300px] gap-2 ml-20">
-                        <h2 className="text-white font-bold">Others</h2>
-                        <Link href="https://sosc.org.in/" className="underline">Website</Link>
-                        <Link href="https://sosc.org.in/guidelines/" className="underline">Community Guidlines</Link>
-                        <Link href="https://github.com/so-sc/code-of-conduct" className="underline">Code of Contact</Link>
-                    </div>
-                </div>
+<div className="md:pl-32 lg:pl-52">
+            <h2 className="text-white font-bold text-xl mb-2">Follow Us</h2>
+            <div className="flex flex-col gap-2">
+              <Link href="https://github.com/so-sc" className="underline text-white">GitHub</Link>
+              <Link href="https://www.instagram.com/sosc.sahyadri/" className="underline text-white">Instagram</Link>
+              <Link href="https://www.linkedin.com/company/sosc-sahyadri/posts/?feedView=all" className="underline text-white">LinkedIn</Link>
+              <Link href="https://x.com/sosc_sahyadri" className="underline text-white">X</Link>
+              <Link href="https://www.youtube.com/channel/UCk8nlSMwUT-jhEtamMF-V-w" className="underline text-white">YouTube</Link>
             </div>
-        </>
-    )
+          </div>
+
+     
+          <div className="md:pl-10 lg:pl-16">
+            <h2 className="text-white font-bold text-xl mb-2">Others</h2>
+            <div className="flex flex-col gap-2">
+              <Link href="https://sosc.org.in" className="underline text-white">Website</Link>
+              <Link href="https://sosc.org.in/guidelines/" className="underline text-white">Community Guidelines</Link>
+              <Link href="https://github.com/so-sc/code-of-conduct" className="underline text-white">Code of Conduct</Link>
+            </div>
+          </div>
+
+        </div>
+
+     
+        <div className="mt-10 border-t border-white/30 pt-4 text-center">
+          <p className="text-white/90 text-sm">
+            © {new Date().getFullYear()} Sahyadri Open Source Community. All rights reserved.
+          </p>
+        </div>
+      </footer>
+    </>
+  );
 }
