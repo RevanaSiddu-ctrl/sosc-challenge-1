f6279e475cdd864153389216cfa2684e9e501258
Sampoorna Prabhu <sampoornaprabhu371@gmail.com>
2025-12-07 17:15:13 +0530

    Fix bugs: swap Events/Blogs nav, fix NavBar/Footer order, fix typos, add missing import, create root page redirect

--- app/compnents/cardB.jsx
+++ app/compnents/cardB.jsx
@@ -1,9 +1,9 @@
 export default function CardB({ image, title, location, date }) {
   return (
-    <div className="flex w-full h-[200px] bg-white rounded-xl shadow-md overflow-hidden cursor-pointer hover:">
+    <div className="flex flex-col md:flex-row w-full h-auto md:h-[200px] bg-white rounded-xl shadow-md overflow-hidden cursor-pointer hover:">
 
       
-      <div className="w-[40%] h-full">
+      <div className="w-full md:w-[40%] h-[150px] md:h-full">
         <img
           src={image}
           alt={title}
@@ -11,7 +11,7 @@ export default function CardB({ image, title, location, date }) {
         />
       </div>
 
-      <div className="w-[60%] p-5 flex flex-col justify-between">
+      <div className="w-full md:w-[60%] p-5 flex flex-col justify-between">
 
         <div>
           <h1 className="text-xl font-bold text-black hover:text-[oklch(77.034%_0.20078_152.412)]">{title}</h1>
