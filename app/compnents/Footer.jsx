0350aeaaf2a7023ab01ce49a792fd7c62bc65548
msd <rehanpatel2194@gmail.com>
2025-12-07 13:10:23 +0530

    First task done fixing all the bugs

--- app/compnents/Footer.jsx
+++ app/compnents/Footer.jsx
@@ -6,31 +6,36 @@ export default function Footer(){
         <>
             <div className="bg-[oklch(77.034%_0.20078_152.412)] w-full h-[400px] flex flex-row mt-10">
         
-                <div className="h-[90%] flex felx-row">
+                <div className="h-[75%] flex felx-row">
 
-                    <div className="flex flex-col justify-center  gap-2 ml-10 w-[100%]">
+                    <div className="flex flex-col justify-center  gap-1 ml-40 w-[100%]">
                         <h2 className="text-white text-2xl text-bold ">Sahyadri Open Source Community</h2>
                         <h4 className="text-white">Sahyadri College of Engineering and Management, Adyar, Mangalore - 575007 IN</h4>
-                        <Link href="https://sosc.org.in" className="underline">sosc.org.in</Link>
+                        <Link href="https://mail.google.com/mail/sosc@sahyadri.edu.in" className="text-white underline">sosc@sahyadri.edu.in</Link>
+                        <h4 className="text-white">+91 79941 34420</h4>
+                        
                     </div>
 
-                    <div className="flex flex-col justify-center items-center w-[140px] gap-2 ml-40">
-                        <h2 className="text-white font-bold">Fallow Us</h2>
-                        <Link href="https://github.com/so-sc" className="underline">GitHub</Link>
-                        <Link href="https://www.instagram.com/sosc.sahyadri/" className="underline">Instagram</Link>
-                        <Link href="https://www.linkedin.com/company/sosc-sahyadri/posts/?feedView=all" className="underline">LinkedIn</Link>
-                        <Link href="https://x.com/sosc_sahyadri?t=y9WZsgmE1Nh5mfq-i6Ne2A&s=08 " className="underline">X</Link>
-                        <Link href="https://www.youtube.com/channel/UCk8nlSMwUT-jhEtamMF-V-w" className="underline">YouTube</Link>
+                    <div className="flex flex-col justify-center items-left w-[140px] gap-2 ml-40">
+                        <h2 className="text-white font-bold">Follow Us</h2>
+                        <Link href="https://github.com/so-sc" className="text-white underline">GitHub</Link>
+                        <Link href="https://www.instagram.com/sosc.sahyadri/" className="text-white underline">Instagram</Link>
+                        <Link href="https://www.linkedin.com/company/sosc-sahyadri/posts/?feedView=all" className="text-white underline">LinkedIn</Link>
+                        <Link href="https://x.com/sosc_sahyadri?t=y9WZsgmE1Nh5mfq-i6Ne2A&s=08 " className="text-white underline">X</Link>
+                        <Link href="https://www.youtube.com/channel/UCk8nlSMwUT-jhEtamMF-V-w" className="text-white underline">YouTube</Link>
                     </div>
 
-                    <div className="flex flex-col justify-center items-center w-[300px] gap-2 ml-20">
+                    <div className="flex flex-col justify-center items-left w-[140px] gap-2 ml-20">
                         <h2 className="text-white font-bold">Others</h2>
-                        <Link href="https://sosc.org.in/" className="underline">Website</Link>
-                        <Link href="https://sosc.org.in/guidelines/" className="underline">Community Guidlines</Link>
-                        <Link href="https://github.com/so-sc/code-of-conduct" className="underline">Code of Contact</Link>
+                        <Link href="https://sosc.org.in/" className="text-white underline">Website</Link>
+                        <Link href="https://sosc.org.in/guidelines/" className="text-white underline">Community Guidlines</Link>
+                        <Link href="https://github.com/so-sc/code-of-conduct" className="text-white underline">Code of Contact</Link>
                     </div>
                 </div>
             </div>
+
+            
+            
         </>
     )
 }
--- app/compnents/NavBar.jsx
+++ app/compnents/NavBar.jsx
@@ -1,22 +1,54 @@
 "use client"
 import Image from "next/image";
-import { useRouter } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
 
 export default function NavBar() {
-    
-  const router = useRouter();
 
-  return(
-        <>
-            <div className="h-[90px] bg-[oklch(77.034%_0.20078_152.412)] w-full flex flex-row  items-center top-0 left-0 z-50">
-                  <Image src="https://sosc.org.in/_astro/sosc_logo_white.Bej07WYV_Z10hDC0.svg" alt="logo" height={0} width={0} className="h-[70px] w-[70px] ml-[50px]"/>
-                  <div className="ml-[1000px] flex  flex-row gap-10">
-                    <button className="text-white text-[2xl] cursor-pointer" onClick={()=>router.push("./home")}>Home</button>
-                  <button className="text-white text-[2xl] cursor-pointer" onClick={()=>router.push("./blogs")}>Events</button>
-                  <button className="text-white text-[2xl] cursor-pointer" onClick={() => router.push("/events")}>Blogs</button>
-                  <button className="text-white text-[2xl] cursor-pointer" onClick={()=>router.push("./team")}>Team</button>
-                  </div>
-                </div>
-        </>
-    )
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const navItems = [
+    { name: "Home", path: "/home" },
+    { name: "Events", path: "/events" },
+    { name: "Blogs", path: "/blogs" },
+    { name: "Team", path: "/team" },
+  ];
+
+  return (
+    <>
+      <div className="h-[50px] bg-[oklch(77.034%_0.20078_152.412)] w-full flex flex-row items-center top-0 left-0 z-50">
+
+        {/* LOGO */}
+        <Image 
+          src="https://sosc.org.in/_astro/sosc_logo_white.Bej07WYV_Z10hDC0.svg" 
+          alt="logo" 
+          height={0} 
+          width={0} 
+          className="h-[70px] w-[70px] ml-[50px]" 
+        />
+
+        {/* NAV LINKS */}
+        <div className="ml-[1000px] flex flex-row gap-10 relative">
+
+          {navItems.map((item) => (
+            <div key={item.path} className="relative">
+              <button
+                className="text-white text-[2xl] cursor-pointer"
+                onClick={() => router.push(item.path)}
+              >
+                {item.name}
+              </button>
+
+              {/* ACTIVE UNDERLINE */}
+              {pathname === item.path && (
+                <div className="absolute left-0 right-0 h-[3px] bg-white mt-1 rounded-full" />
+              )}
+            </div>
+          ))}
+
+        </div>
+
+      </div>
+    </>
+  );
 }
