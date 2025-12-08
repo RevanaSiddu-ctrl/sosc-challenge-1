d51464f0cb832b5f0388193c006e2e2626dc5021
ctutorials123 <varshapoojari45@gmail.com>
2025-12-08 23:53:20 +0530

    Fix Issue #3: add team data file and import it

---
+++ app/team/teamData.js
@@ -0,0 +1,34 @@
+const teamData = [
+  {
+    id: 1,
+    name: "Akshay",
+    role: "Lead",
+    image: "https://sosc.org.in/team/akshay.jpg",
+  },
+  {
+    id: 2,
+    name: "Harshith",
+    role: "Community Lead",
+    image: "https://sosc.org.in/team/harshith.png",
+  },
+  {
+    id: 3,
+    name: "Harshitha",
+    role: "Design Lead",
+    image: "https://sosc.org.in/team/harshitha.jpg",
+  },
+  {
+    id: 4,
+    name: "Nithin",
+    role: "Open Source Contributor",
+    image: "https://sosc.org.in/team/nithin.jpg",
+  },
+  {
+    id: 5,
+    name: "Vishwas",
+    role: "Backend Developer",
+    image: "https://sosc.org.in/team/vishwas.jpg",
+  }
+];
+
+export default teamData;
